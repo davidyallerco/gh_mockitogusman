5b4e9244a3e546f3e0fc17fffa1a43acef6f9a5e
davidyallerco <david.yallerco@gmail.com>
2023-01-17 12:22:49 -0500

    v5 probando con un metodo con lista vacia, no pasa la prueba

--- src/test/java/pe/parnertdigital/mockitoguzman/services/ExamenServiceImplTest.java
+++ src/test/java/pe/parnertdigital/mockitoguzman/services/ExamenServiceImplTest.java
@@ -7,6 +7,7 @@ import pe.parnertdigital.mockitoguzman.repositories.ExamenRepository;
 import pe.parnertdigital.mockitoguzman.repositories.ExamenRepositoryOtro;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,8 +17,8 @@ import static org.mockito.Mockito.*;
 class ExamenServiceImplTest {
 
     @Test
-    void buscarExamenPorNombre() {
-        ExamenRepository repository = mock(ExamenRepository.class);//supuestamente usando la interface
+    void buscarExamenPorNombreTest() {
+        ExamenRepository repository = mock(ExamenRepository.class);
         ExamenService service = new ExamenServiceImpl(repository);
         List<Examen> datosSimulados =    Arrays.asList(
                 new Examen(5L, "Matematicas"),
@@ -28,7 +29,21 @@ class ExamenServiceImplTest {
         Optional<Examen> examen = service.buscarExamenPorNombre("Matematicas");
 
         assertTrue(examen.isPresent());
-        assertEquals(5L, examen.orElseThrow(null).getId());//null le puse en java 8
-        assertEquals("Matematicas", examen.get().getNombre());//otra forma, tambien lo puedes hacer como arriba
+        assertEquals(5L, examen.orElseThrow(null).getId());
+        assertEquals("Matematicas", examen.get().getNombre());
+    }
+
+    @Test
+    void buscarExamenPorNombreListaVaciaTest() {
+        ExamenRepository repository = mock(ExamenRepository.class);
+        ExamenService service = new ExamenServiceImpl(repository);
+        List<Examen> datosSimulados = Collections.emptyList();
+
+        when(repository.buscarTodos()).thenReturn(datosSimulados);
+        Optional<Examen> examen = service.buscarExamenPorNombre("Matematicas");
+
+        assertTrue(examen.isPresent());
+        assertEquals(5L, examen.orElseThrow(null).getId());
+        assertEquals("Matematicas", examen.get().getNombre());
     }
 }
