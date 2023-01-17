fbae1184a385e160f0627afec99d514b46b4d60b
davidyallerco <david.yallerco@gmail.com>
2023-01-17 11:48:26 -0500

    v3 probando con mockito con su propia data simulda

--- src/test/java/pe/parnertdigital/mockitoguzman/services/ExamenServiceImplTest.java
+++ src/test/java/pe/parnertdigital/mockitoguzman/services/ExamenServiceImplTest.java
@@ -1,18 +1,29 @@
 package pe.parnertdigital.mockitoguzman.services;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import pe.parnertdigital.mockitoguzman.models.Examen;
 import pe.parnertdigital.mockitoguzman.repositories.ExamenRepository;
-import pe.parnertdigital.mockitoguzman.repositories.ExamenRepositoryImpl;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ExamenServiceImplTest {
 
     @Test
     void buscarExamenPorNombre() {
-        ExamenRepository repository = new ExamenRepositoryImpl();
+        ExamenRepository repository = mock(ExamenRepository.class);//pasamos el nombre de la clase o interface a simular
         ExamenService service = new ExamenServiceImpl(repository);
+        List<Examen> datosSimulados =    Arrays.asList(
+                new Examen(5L, "Matematicas"),
+                new Examen(6L, "Lenguaje"),
+                new Examen(7L, "Historia")
+        );//incluso lo eliminamos la clase examenRespositoryImpl
+        //cuando se invoque el metodo del repository entonces devolver los datos simulados
+        when(repository.buscarTodos()).thenReturn(datosSimulados);
         Examen examen = service.buscarExamenPorNombre("Matematicas");
         assertNotNull(examen);
         assertEquals(5L, examen.getId());
