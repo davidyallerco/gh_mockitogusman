d62d844aa3f667877886249907f0714e66bc255b
davidyallerco <david.yallerco@gmail.com>
2023-01-17 11:29:53 -0500

    v2 vemos el primer test con junit sin ayuda de mockito 2

--- src/main/java/pe/parnertdigital/mockitoguzman/repositories/ExamenRepositoryImpl.java
+++ src/main/java/pe/parnertdigital/mockitoguzman/repositories/ExamenRepositoryImpl.java
@@ -3,15 +3,19 @@ package pe.parnertdigital.mockitoguzman.repositories;
 import pe.parnertdigital.mockitoguzman.models.Examen;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ExamenRepositoryImpl implements ExamenRepository{
     @Override
     public List<Examen> buscarTodos() {
-        return Arrays.asList(
+       /* return Arrays.asList(
                 new Examen(5L, "Matematicas"),
                 new Examen(6L, "Lenguaje"),
                 new Examen(7L, "Historia")
         );
+        */
+        //si queremos probar una lista vacia, tendremos que modificar, con mockito evitariamos esto
+        return Collections.emptyList();
     }
 }
